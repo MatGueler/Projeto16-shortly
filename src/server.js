43b45810dee36d5edb2e2eb758fdf69baf6f4bbb
MattGueler <mateus.gueler@gmail.com>
2022-08-02 16:20:20 -0300

    Cria server e chama as rotas

---
+++ src/server.js
@@ -0,0 +1,19 @@
+import express from 'express'
+import dotenv from 'dotenv'
+import cors from 'cors'
+
+dotenv.config();
+
+const server = express();
+server.use(express.json());
+server.use(cors());
+
+server.use(userRouter)
+
+server.use(urlRouter)
+
+server.use(rankingRoter)
+
+server.listen(process.env.PORT, () => {
+    console.log("Server running on port " + process.env.PORT);
+});
