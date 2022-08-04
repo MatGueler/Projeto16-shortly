a70a15004960272c4248601375504a234c6d3d78
MattGueler <mateus.gueler@gmail.com>
2022-08-04 01:44:03 -0300

    Reduz a url e relaciona com o usuário que a criou

--- src/Controllers/urlController.js
+++ src/Controllers/urlController.js
@@ -1,4 +1,5 @@
 import connection from '../dbStrategy/postgres.js'
+import { nanoid } from 'nanoid/non-secure'
 import joi from 'joi'
 
 export async function EncodeUrl(req, res, next) {
@@ -7,8 +8,75 @@ export async function EncodeUrl(req, res, next) {
         const { id } = res.locals.dados
         const url = res.locals.url
 
-        console.log(id)
-        return res.send(200)
+        let shortUrl;
+
+        const userSchema = joi.object({
+            url: joi.string().uri().required()
+        });
+
+        const validation = userSchema.validate({ url }, { abortEarly: true });
+
+        if (validation.error) {
+            let err = validation.error.details[0].message
+            return res.status(422).send(err)
+        }
+
+        const { rows: myUrl } = await connection.query
+            (`SELECT * FROM urls
+        WHERE url=$1`
+                , [url])
+
+        const urlNotExist = myUrl.length === 0;
+
+        // Para o caso dessa url ainda nao ter sido reduzida
+        if (urlNotExist) {
+
+            shortUrl = nanoid()
+
+            await connection.query(`INSERT INTO urls (url) 
+            VALUES ($1)`
+                , [url]);
+
+            const { rows: myUrl } = await connection.query
+                (`SELECT * FROM urls
+            WHERE url=$1`
+                    , [url])
+
+            const urlId = myUrl[0].id
+
+            await connection.query(`INSERT INTO "shortUrls" ("urlId","shortUrl") 
+            VALUES ($1,$2)`
+                , [urlId, shortUrl]);
+
+        }
+
+        const urlId = myUrl[0].id
+
+        const { rows: myShortUrl } = await connection.query
+            (`SELECT * FROM "shortUrls"
+        WHERE "urlId"=$1`
+                , [urlId])
+
+        const urlShortId = myShortUrl[0].id
+
+        const { rows: myUserShort } = await connection.query
+            (`SELECT * FROM "usersShort"
+        WHERE id=$1 AND "shortUrlId"=$2`
+                , [id, urlShortId])
+
+        if (myUserShort.length === 0) {
+            await connection.query(`INSERT INTO "usersShort" ("userId","shortUrlId") 
+            VALUES ($1,$2)`
+                , [id, urlShortId]);
+
+            shortUrl = myShortUrl[0].shortUrl
+        }
+
+        const body = {
+            shortUrl
+        }
+
+        return res.status(201).send(shortUrl)
     }
     catch {
         return res.send(500)
