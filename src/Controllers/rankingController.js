ea0b542f85aa7d81454b3ebb9832e92935d3d580
MattGueler <mateus.gueler@gmail.com>
2022-08-05 19:49:23 -0300

    Retorna o ranking de usuarios

---
+++ src/Controllers/rankingController.js
@@ -0,0 +1,21 @@
+
+import connection from '../dbStrategy/postgres.js'
+
+export async function rankingUsers(req, res) {
+
+    try {
+
+        const { rows: ranking } = await connection.query
+            (`SELECT users.id,users.name,COUNT(su."shortUrl") AS "linksCount",SUM(v.view) AS "visitCount" FROM views v
+            JOIN "shortUrls" su ON v."shortUrlId"=su.id
+            JOIN users ON su."userId"=users.id
+            GROUP BY users.name,users.id
+            ORDER BY "visitCount" DESC`)
+
+        return res.status(200).send(ranking)
+    }
+    catch {
+        return res.send(500)
+    }
+
+}
