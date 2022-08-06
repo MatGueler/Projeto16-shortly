fdb6687c30cb117bf4ee127e88da2ca0d9369ca0
MattGueler <mateus.gueler@gmail.com>
2022-08-06 00:34:36 -0300

    Teste heroku

--- src/Controllers/userController.js
+++ src/Controllers/userController.js
@@ -85,7 +85,7 @@ export async function loginUser(req, res) {
 
         const dados = { id: users[0].id };
         const chaveSecreta = process.env.JWT_SECRET;
-        // const token = jwt.sign(dados, chaveSecreta);
+        const token = jwt.sign(dados, chaveSecreta);
 
         return res.send(200)
     }
