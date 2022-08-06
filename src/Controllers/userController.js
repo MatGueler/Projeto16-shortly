9dd1bcf0d160de7fdf1c1f901da218ce0b7c4f92
MattGueler <mateus.gueler@gmail.com>
2022-08-06 00:16:39 -0300

    Teste heroku

--- src/Controllers/userController.js
+++ src/Controllers/userController.js
@@ -59,35 +59,35 @@ export async function loginUser(req, res) {
 
         // const cryptPassword = 
 
-        const { rows: users } = await connection.query
-            (`SELECT * FROM users
-        WHERE email=$1`
-                , [email])
+        // const { rows: users } = await connection.query
+        //     (`SELECT * FROM users
+        // WHERE email=$1`
+        //         , [email])
 
-        const noExist = users.length === 0;
+        // const noExist = users.length === 0;
 
-        const userSchema = joi.object({
-            email: joi.string().email().required(),
-            password: joi.string().min(8).required(),
-        });
+        // const userSchema = joi.object({
+        //     email: joi.string().email().required(),
+        //     password: joi.string().min(8).required(),
+        // });
 
-        const validation = userSchema.validate({ email, password }, { abortEarly: true });
+        // const validation = userSchema.validate({ email, password }, { abortEarly: true });
 
-        if (validation.error || noExist) {
-            let err;
-            if (noExist) {
-                err = "This account doesn't exist"
-                return res.status(401).send(err)
-            }
-            if (validation.error) {
-                err = validation.error.details[0].message
-                return res.status(422).send(err)
-            }
-        }
+        // if (validation.error || noExist) {
+        //     let err;
+        //     if (noExist) {
+        //         err = "This account doesn't exist"
+        //         return res.status(401).send(err)
+        //     }
+        //     if (validation.error) {
+        //         err = validation.error.details[0].message
+        //         return res.status(422).send(err)
+        //     }
+        // }
 
-        const dados = { id: users[0].id };
-        const chaveSecreta = process.env.JWT_SECRET;
-        const token = jwt.sign(dados, chaveSecreta);
+        // const dados = { id: users[0].id };
+        // const chaveSecreta = process.env.JWT_SECRET;
+        // const token = jwt.sign(dados, chaveSecreta);
 
         // await connection.query(`INSERT INTO tokens (token) 
         // VALUES ($1)`
