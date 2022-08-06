61d55b17896eb4bb3b1ff1dc998efdfe7b76f540
MattGueler <mateus.gueler@gmail.com>
2022-08-05 22:49:49 -0300

    Ordena o ranking incluindo pessoas sem urls criadas e sem visualizações

--- src/Controllers/rankingController.js
+++ src/Controllers/rankingController.js
@@ -6,7 +6,7 @@ export async function rankingUsers(req, res) {
     try {
 
         const { rows: ranking } = await connection.query
-            (`SELECT users.id,users.name,COUNT(su."shortUrl") AS "linksCount",SUM(views.view) AS "visitCount" FROM  users
+            (`SELECT users.id,users.name,COUNT(su."shortUrl") AS "linksCount",COALESCE(SUM(views.view),0) AS "visitCount" FROM  users
             LEFT JOIN "shortUrls" su ON su."userId"=users.id
             LEFT JOIN views ON su.id=views."shortUrlId"
             GROUP BY users.id
