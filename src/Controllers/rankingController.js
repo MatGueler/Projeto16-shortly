84e737541b791bd59d684d4ad643c5408b88c974
MattGueler <mateus.gueler@gmail.com>
2022-08-07 18:53:20 -0300

    teste url criada

--- src/Controllers/rankingController.js
+++ src/Controllers/rankingController.js
@@ -10,7 +10,7 @@ export async function rankingUsers(req, res) {
             LEFT JOIN "shortUrls" su ON su."userId"=users.id
             LEFT JOIN views ON su.id=views."shortUrlId"
             GROUP BY users.id
-            ORDER BY "visitCount" DESC LIMIT 1`)
+            ORDER BY "visitCount" DESC LIMIT 10`)
 
         return res.status(200).send(ranking)
     }
